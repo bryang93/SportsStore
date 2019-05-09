a9dc3a043401ad2f89e7df1b4fdc9f0473fa673d
Brayan Gudino <gudino1993@hotmail.com>
2019-05-09 09:39:47 -0400

    SportsStore1B

---
+++ SportsStore.Domain/Abstract/IProductsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SportsStore.Domain.Abstract
+{
+    public interface IProductsRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
---
+++ SportsStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("284b4061-0e21-41b0-af51-d2c85029ef57")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
